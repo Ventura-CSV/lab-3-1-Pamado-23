91e1a1fbfe8294a13f46c384d15155121f81c54b
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-27 15:50:11 -0700

    Lab 3-1

--- main.py
+++ main.py
@@ -7,6 +7,23 @@ def main():
     """
     Make your code here
     """
+    
+    # If the number is less than 50, set the variable range to 1
+
+    if number < 50:
+        range = 1 
+
+    # If the number is greater than or equal to 50 and less than 100, set the variable range to 2
+
+    elif 50 <= number < 100:
+        range = 2
+
+    # If the number is greater than or equal to 100, set the variable range to 3
+
+    else:
+        range = 3
+
+
 
     print(f'Range is {range}')
     ########################################
